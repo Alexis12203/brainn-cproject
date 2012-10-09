ef8fcbb26f9823d443ded0bb5317a6f274994657
R0ckET <leohidalgo@gmail.com>
2012-10-09 17:12:49 -0300

    Implementa método para la carga dinámica de páginas

--- js/default.js
+++ js/default.js
@@ -1,4 +1,14 @@
 var Default = {};
 
+Default.load_page = function (url) {
+	if (typeof url != 'string') return false;
+
+	var filename = '/view' + url + '.ctp';
+
+	$.get(filename, function (data) {
+		document.getElementById('content').innerHTML = data;
+	});
+};
+
 (function () {
 });
